87284a1180af666ab8503e8403f4e9e2e8823d8f
orzehhowski <filmyminecraftnayt@gmail.com>
2025-06-16 19:52:20 +0200

    sqlite db implemented

--- api/main.py
+++ api/main.py
@@ -2,64 +2,82 @@ import datetime
 import zipfile
 import requests
 from .lib import *
+from . import sqlite
 from dotenv import load_dotenv
 import os
 
 
 if (__name__=="__main__"):
   load_dotenv()
-
   url = os.environ["GTFS_URL"]
   output_filename = "gtfs.zip"
   data_dir = "gtfs"
 
-  # get data from ztm api
-  response = requests.get(url, headers={
-    "Content-Type": "application/x-www-form-urlencoded", 
-    "Accept": "application/octet-stream"
-  })
+  # data processing flags - for testing
+  download_new_data = False
+  save_data_in_db = True
 
-  if (response.status_code != 200):
-    print(f"Response failed: {response.status_code}")
-    quit()
 
-  with open(output_filename, "wb") as output_file:
-    output_file.write(response.content)
+  # 1. get data from ztm api
+  if download_new_data:
+    response = requests.get(url, headers={
+      "Content-Type": "application/x-www-form-urlencoded", 
+      "Accept": "application/octet-stream"
+    })
 
-  # unzip data
+    if (response.status_code != 200):
+      print(f"Response failed: {response.status_code}")
+      quit()
+
+    with open(output_filename, "wb") as output_file:
+      output_file.write(response.content)
+
+
+  # 2. unzip data
+
   os.makedirs(data_dir, exist_ok=True)
   
-  with zipfile.ZipFile(output_file, "r") as zip_ref:
+  with zipfile.ZipFile(output_filename, "r") as zip_ref:
     zip_ref.extractall(data_dir)
 
-  # save data to sqlite
-  now = datetime.datetime.now().strftime("%H:%M:%S")
+
+  # 3. save data to sqlite
+
   now_weekday = datetime.datetime.now().strftime("%w")
+  now = datetime.datetime.now().strftime("%H:%M:%S")
 
   # there's no need to save calendars to db - it is needed only once a day, after data fetch
-  todays_calendar = read_todays_calendar(data_dir + "/calendar.txt", now_weekday)
+  todays_calendar = read_todays_calendar(os.path.join(data_dir, "calendar.txt"), WEEKDAYS[int(now_weekday)])
+  print(todays_calendar)
 
-  # current_calendar = read_todays_calendar("./data/calendar.txt", WEEKDAYS[int(now_weekday)])
+  if save_data_in_db:
 
-  # trip_h, trips = read_trips("./data/trips.txt", current_calendar)
+    # clear db before saving new data
+    if os.path.exists(os.environ["DB_FILENAME"]):
+      print("pruning database...")
+      sqlite.prune_db()
 
-  # stop_times_h, stop_times = read_stop_times("./data/stop_times.txt")
+    sqlite.init_db()
 
-  
-  
-  
-  # join trips and stop_times 
-  # final_data = []
-  # for stop_time in stop_times:
-  #   for trip in trips:
-  #     if stop_time["trip_id"] == trip["trip_id"]:
-  #       final_data.append({
-  #         "route_id": trip["route_id"],
-  #         "trip_headsign": stop_time["stop_headsign"] or trip["trip_headsign"],
-  #         "departure_time": stop_time["departure_time"]
-  #       })
+    # read todays trips from file and save it to db
+    print("Reading trips from file...")
+    trips_h, trips = read_trips(os.path.join(data_dir, "trips.txt"), todays_calendar)
 
-  # final_data.sort(key= lambda x: x["departure_time"])
+    print("Saving trips to db...")
+    sqlite.create_trips(trips)
 
-  # for data in final_data[:20]:
-  #   print(*data.values(), sep="\t")
+    # read stop_times from file and save it to db
+    print("Reading stop times from file...")
+    stop_times_h, stop_times = read_stop_times(os.path.join(data_dir, "stop_times.txt"))
+    
+    print("Saving stop times to db...")
+    sqlite.create_stop_times(stop_times)
+
+
+  # 4. get data from db
+  today_departures = sqlite.get_todays_stop_departures("60", todays_calendar)
+
+  today_departures = [x for x in today_departures if x[2] > now]
+
+  for data in today_departures[:20]:
+    print(*data, sep="\t")
--- api/sqlite.py
+++ api/sqlite.py
@@ -1,7 +1,8 @@
 import sqlite3
+import os
 
 def get_connection():
-  return sqlite3.connect("schedules.db")
+  return sqlite3.connect(os.environ["DB_FILENAME"])
 
 def init_db():
   with get_connection() as conn:
@@ -24,34 +25,48 @@ def init_db():
                    """)
     conn.commit()
 
-def create_trip(trip_id, route_id, trip_headsign):
+def create_trips(trips: list[dict]):
   with get_connection() as conn:
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO trips (trip_id, route_id, trip_headsign) VALUES (?, ?, ?)", 
-                    (trip_id, route_id, trip_headsign))
+    data = [(trip["trip_id"], trip["route_id"], trip["trip_headsign"]) for trip in trips]
+    cursor.executemany("INSERT INTO trips (trip_id, route_id, trip_headsign) VALUES (?, ?, ?)", 
+                    data)
     conn.commit()
 
-def create_stop_time(trip_id, stop_id, departure_time, stop_headsign):
-   with get_connection() as conn:
-     cursor = conn.cursor()
-     cursor.execute("INSERT INTO trips (trip_id, stop_id, departure_time, stop_headsign) VALUES (?, ?, ?, ?)",
-                    (trip_id, stop_id, departure_time, stop_headsign))
-     conn.commit()
+def create_stop_times(stop_times: list[dict]):
+  with get_connection() as conn:
+    cursor = conn.cursor()
+    data = [(st["trip_id"], st["stop_id"], st["departure_time"], st["stop_headsign"]) for st in stop_times]
+    cursor.executemany("INSERT INTO stop_times (trip_id, stop_id, departure_time, stop_headsign) VALUES (?, ?, ?, ?)",
+                  data)
+    conn.commit()
 
 def get_todays_stop_departures(stop_id, current_calendar):
   with get_connection() as conn:
     cursor = conn.cursor()
-    cursor.execute("""SELECT trips.route_id, trips.trip_headsign, stop_times.stop_headsign, stop_times.departure_time
+    cursor.execute("""SELECT trips.route_id, stop_times.stop_headsign, stop_times.departure_time
                    from stop_times
                    inner join trips on trips.trip_id = stop_times.trip_id
                    where stop_times.stop_id = ? and stop_times.trip_id like ?
-                   """, (stop_id, f"{current_calendar}%"))
+                   order by stop_times.departure_time
+                   """, (stop_id, f"\"{current_calendar}%"))
+    return cursor.fetchall()
+  
+def get_trips_sample(limit=10):
+  with get_connection() as conn:
+    cursor = conn.cursor()
+    cursor.execute("select * from trips limit ?", (str(limit),))
+    return cursor.fetchall()
+  
+def get_stop_times_sample(limit=10):
+  with get_connection() as conn:
+    cursor = conn.cursor()
+    cursor.execute("select * from stop_times limit ?", (str(limit),))
     return cursor.fetchall()
   
 def prune_db():
   with get_connection() as conn:
     cursor = conn.cursor()
-    cursor.execute("delete from calendar")
     cursor.execute("delete from trips")
     cursor.execute("delete from stop_times")
     conn.commit()
